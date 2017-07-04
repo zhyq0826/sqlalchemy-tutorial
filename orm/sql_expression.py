5c5af6fc0f89165cac9203ffb542722f5d9e03ce
zhyq0826 <zhyq0826@126.com>
2017-07-04 19:19:02 +0800

    update

---
+++ orm/sql_expression.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# noqa
+import random
+import time
+from db import engine, DBSession
+from model import Tag
+
+from sqlalchemy import (
+    func,
+    or_,
+    and_,
+)
+from sqlalchemy import inspect
+from sqlalchemy.sql import select
+from sqlalchemy.sql import text
+
+
+def sql_in():
+    session = DBSession()
+    sql = text("select * from tag where id in :id")
+    print session.execute(sql, {'id': [10, 11, 12]}).fetchall()
+
+
+if __name__ == '__main__':
+    sql_in()
